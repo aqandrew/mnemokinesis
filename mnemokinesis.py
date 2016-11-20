5f1836c43524faeb38cd12ad60799888405c1afa
dawneraq <dawneraq@gmail.com>
2016-11-20 18:19:31 -0500

    Create mnemokinesis.py

---
+++ mnemokinesis.py
@@ -0,0 +1,15 @@
+"""
+mnemokinesis.py
+
+CSCI-4210 Operating Systems F16
+
+Python simulation of a variety of memory management systems, including
+contiguous, non-contiguous, and virtual memory.
+"""
+
+def main():
+	#TODO Check command line arguments.
+
+
+if __name__ == '__main__':
+	main()
